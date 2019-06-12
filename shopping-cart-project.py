33da6bfc255252ae091fcc3669dce3300f383bbd
eocello10 <50848324+eocello10@users.noreply.github.com>
2019-06-11 21:25:16 -0400

    Added notes for next steps

--- shopping-cart-project.py
+++ shopping-cart-project.py
@@ -84,12 +84,12 @@ for Selected_ID in selected_ids:
 print("Total Price: " + str(total_price))
 ####price_usd = " (${0:.2f})".format(p["price"]) - Need to format/define p - check groceries
 
-#A grocery store name of your choice
-#A grocery store phone number and/or website URL and/or address of choice
-#The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
-#The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
+#A grocery store name of your choice - Print name
+#A grocery store phone number and/or website URL and/or address of choice - Print Url
+#The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM) - ??? How do we know specific date. Look up date time code discuss in class
+#The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50) - Need to change definition of p
 #The total cost of all shopping cart items, formatted as US dollars and cents (e.g. $4.50), calculated as the sum of their prices
 #The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
 #The total amount owed, formatted as US dollars and cents (e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
-#A friendly message thanking the customer and/or encouraging the customer to shop again
+#A friendly message thanking the customer and/or encouraging the customer to shop again - Print a friendly message 
 #The program should be able to process multiple shopping cart items of the same kind, but need not display any groupings or aggregations of those items (although it may optionally do so).
